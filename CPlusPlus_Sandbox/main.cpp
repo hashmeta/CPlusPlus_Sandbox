0f1bee448ea79873659a058ae4da5285eee0f34a
hashmeta <ruinedmaniac@gmail.com>
2023-04-07 13:20:28 +0530

    Initial Commit

---
+++ CPlusPlus_Sandbox/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CPlusPlus_Sandbox
+//
+//  Created by Kalpesh Kadam on 07/04/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!    \n";
+    return 0;
+}
